d94e34a9f7d97bf3c5d6312e8b09633acfa9c04d
Jade T <jade@tinkrtech.net>
2025-12-04 14:38:11 -0500

    Extract networking to module

---
+++ configs/network.nix
@@ -0,0 +1,57 @@
+{ config, pkgs, inputs, ... }:
+{
+	networking = {
+		hostName = "nixos-laptop";
+		networkmanager = {
+			enable = true;
+			plugins = [ pkgs.networkmanager-openvpn ];
+		};
+		firewall = {
+			logReversePathDrops = true; # Show dropped packets in dmesg 
+			# Ignore Wireguard traffic (rpfilter gets confused)
+			extraCommands = ''
+				ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN
+			'';
+			extraStopCommands = ''
+				ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN || true
+				ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN || true
+			'';
+		};
+		
+	};
+
+	services.openvpn.servers = {
+		pia = {
+			autoStart = false;
+			authUserPass = {
+				username = config.sops.secrets."pia/username".path;
+				password = config.sops.secrets."pia/password".path;
+			};
+			
+			config = ''
+				client
+				dev tun
+				proto udp
+				remote ca-ontario-so.privacy.network 1197
+				resolv-retry infinite
+				nobind
+				persist-key
+				persist-tun
+				cipher aes-256-cbc
+				auth sha256
+				tls-client
+				remote-cert-tls server
+
+				auth-user-pass
+				compress
+				verb 1
+				reneg-sec 0
+
+				crl-verify ${config.sops.secrets."pia/ontario-so-crl".path}
+				ca ${config.sops.secrets."pia/ontario-so-ca".path}
+
+				disable-occ
+			'';
+		};
+	};	
+}
--- configuration.nix
+++ configuration.nix
@@ -3,6 +3,7 @@
 	imports = [ 
 		./hardware-configuration.nix
 		./configs/nixvim.nix
+		./configs/network.nix
 		inputs.sops.nixosModules.sops
 	];
 
@@ -13,61 +14,6 @@
 		kernelPackages = pkgs.linuxPackages_latest;
 	};
 	systemd.tpm2.enable = false;
-
-	networking = {
-		hostName = "nixos-laptop";
-		networkmanager = {
-			enable = true;
-			plugins = [ pkgs.networkmanager-openvpn ];
-		};
-		firewall = {
-			logReversePathDrops = true; # Show dropped packets in dmesg 
-			# Ignore Wireguard traffic (rpfilter gets confused)
-			extraCommands = ''
-				ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN
-			'';
-			extraStopCommands = ''
-				ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN || true
-				ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN || true
-			'';
-		};
-		
-	};
-
-	services.openvpn.servers = {
-		pia = {
-			autoStart = false;
-			authUserPass = {
-				username = config.sops.secrets."pia/username".path;
-				password = config.sops.secrets."pia/password".path;
-			};
-			
-			config = ''
-				client
-				dev tun
-				proto udp
-				remote ca-ontario-so.privacy.network 1197
-				resolv-retry infinite
-				nobind
-				persist-key
-				persist-tun
-				cipher aes-256-cbc
-				auth sha256
-				tls-client
-				remote-cert-tls server
-
-				auth-user-pass
-				compress
-				verb 1
-				reneg-sec 0
-
-				crl-verify ${config.sops.secrets."pia/ontario-so-crl".path}
-				ca ${config.sops.secrets."pia/ontario-so-ca".path}
-
-				disable-occ
-			'';
-		};
-	};
 	
 	# Set your time zone.
 	time.timeZone = "America/Toronto";
