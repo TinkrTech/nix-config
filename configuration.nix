4d1487438dfb23a79730111f76af7fc40f7af7a4
Jade T <jade@tinkrtech.net>
2026-05-27 12:02:33 -0400

    100: Disable TPM fix

--- configuration.nix
+++ configuration.nix
@@ -16,6 +16,14 @@
 		supportedFilesystems = [ "nfs" ];
 	};
 	security.tpm2.enable = false;
+	boot.initrd.systemd.units = {
+		"dev-tpm0.device".enable = false;
+		"dev-tpmrm0.device".enable = false;
+	};
+	systemd.units = {
+		"dev-tpm0.device".enable = false;	
+		"dev-tpmrm0.device".enable = false;
+	};	
 
 	fileSystems."/mnt/vanasa" = {
 		fsType = "nfs";
