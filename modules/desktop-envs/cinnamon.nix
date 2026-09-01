5ada87fd74052c73faf4925f267c67ab504be151
Jade T <jade@tinkrtech.net>
2026-09-01 16:10:22 -0400

    Add librewolf to cinnamon default config

--- modules/desktop-envs/cinnamon.nix
+++ modules/desktop-envs/cinnamon.nix
@@ -6,4 +6,8 @@
 	# Enable the Cinnamon Desktop Environment.
 	services.xserver.displayManager.lightdm.enable = true;
 	services.xserver.desktopManager.cinnamon.enable = true;
+	
+	environment.systemPackages = with pkgs; [
+		librewolf
+	];	
 }
