75a73998b122ed787b613019d086009c8f52d913
Jade T <jade@tinkrtech.net>
2026-09-03 15:22:06 -0400

    lopen-116: Add git aliases and enable push.autoSetupRemote

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -9,11 +9,25 @@
 			};
 			init.defaultBranch = "main";
 			core.sshCommand = "ssh -i ~/.ssh/github";
+			rebase.autosquash = true;
+
 			alias = {
+				alias = "config --get-regexp ^alias";
+				contributors = "shortlog -sne --all";
+				drag = "blame -wCCC";
+				fixup = "commit --fixup HEAD";
 				lg = "log --oneline";
 				lgbt = "lg --graph main..HEAD";
-				fixup = "commit --fixup HEAD";
+				cdiff = ''!f() { git diff "$1~1"..$1 --compact-summary; }; f'';
+				wdiff = ''!f() { git diff "$1~1"..$1 --word-diff; }; f'';
+				rebase-branch = ''!f() { git rebase -i $(git merge-base main HEAD); }; f'';
+				rename = ''!rename() {
+					git branch --unset-upstream && git push origin -d "$1";
+					git branch -m "$1" "$2" && git push origin -u "$2";
+				}; rename'';
 			};
+
+			push.autoSetupRemote = true;
 		};
 	};
 }
