4975088e6bc84819b46fcb3c4720facf1ddf4f8d
Jade T <jade@tinkrtech.net>
2026-09-24 11:06:46 -0400

    lopen-118: Update flake

--- flake.nix
+++ flake.nix
@@ -6,8 +6,8 @@
 			url = "github:nix-community/nixvim/nixos-26.05";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
-		homemanager = {
-			url = "github:nix-community/home-manager?ref=release-26.05";
+		home-manager = {
+			url = "github:nix-community/home-manager/release-26.05";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 		sops = {
